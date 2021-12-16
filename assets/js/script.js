a99475b0beddc7057f03bd433a4618d936c5bf41
rrich-kray <rrich.kray@gmail.com>
2021-12-15 20:59:00 -0500

    added task warning highlights

--- assets/js/script.js
+++ assets/js/script.js
@@ -257,7 +257,7 @@ var auditTask = function(taskEl) {
   var time = moment(date, "L").set("hour", 17);
 
   // remove any old classes from element
-  $(taskEl).removeClass("list-group-item-warning list-grou-item-danger");
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
 
   // apply new class if task is near/over due date
   if (moment().isAfter(time)) {
